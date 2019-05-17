95f08fffede9158de855cd3daacd11d19767b12b
mesantamaria <mesantamaria@uc.cl>
2019-05-17 12:26:02 -0400

    Número de nodos en consenso imlementado

--- código/Simulator.py
+++ código/Simulator.py
@@ -1,6 +1,7 @@
 import random
 from Graph import Graph
 from Transaction import Transaction
+from collections import defaultdict
 
 
 class Simulator:
@@ -15,20 +16,38 @@ class Simulator:
 		transactions = [Transaction(random.randint(1, 50)) for i in range(10)]
 		return transactions
 
-	def run(self):
-		# 1 ronda
-		transactions = self.generate_transactions()
-		for transaction in transactions:
-			for node in self.graph.get_nodes():
-				if random.random() < self.prob_receive_message:
-					print("Nodo {} recibe mensaje {} para propagar".format(node.id, transaction.uniqueID))
-					self.graph.propagate_message(node, transaction)	
-			
-			
-		
+	def nodes_in_consensus(self):
+		#sets_of_transactions = defaultdict(0)
+		sets_of_transactions = []
+		number_of_nodes = []
+		for node in self.graph.get_nodes():
+			if node.get_transactions() not in sets_of_transactions:
+				sets_of_transactions.append(node.get_transactions())
+				number_of_nodes.append(1)
+			else:
+				index = sets_of_transactions.index(node.get_transactions())
+				number_of_nodes[index] += 1
+		return max(number_of_nodes)
+
+	def print_nodes_info(self):
 		for node in self.graph.get_nodes():
 			print("NODO {}".format(node.id))
 			node.print_transactions()
+			
+
+	def run(self):
+		
+		for round in range(self.rounds):
+			# 1 ronda	
+			transactions = self.generate_transactions()
+			for transaction in transactions:
+				for node in self.graph.get_nodes():
+					if random.random() < self.prob_receive_message:
+						print("Nodo {} recibe mensaje {} para propagar".format(node.id, transaction.uniqueID))
+						self.graph.propagate_message(node, transaction)	
+				
+			# self.print_nodes_info()	
+			print("Máximo número de nodos en consenso: {}".format(self.nodes_in_consensus()))
 
 
 
@@ -38,7 +57,7 @@ if __name__ == "__main__":
 	n1 = graph.add_node()
 	n2 = graph.add_node()
 	n3 = graph.add_node()
-	n4 = graph.add_node(1)
+	n4 = graph.add_node()
 	n5 = graph.add_node()
 	n6 = graph.add_node()
 	graph.add_connection(n1, n2)
@@ -47,6 +66,6 @@ if __name__ == "__main__":
 	graph.add_connection(n4, n5)
 	graph.add_connection(n5, n6)
 	graph.add_connection(n6, n1)
-	simulator = Simulator(graph, 0.4, 0.5, 0.1, 1)
+	simulator = Simulator(graph, 0.4, 0.5, 0.1, 10)
 
 	simulator.run()
